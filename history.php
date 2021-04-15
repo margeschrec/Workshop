e2afa30d73d784ae24e5a63dc4a95a5378923e0c
margeschrec <margeschrec@gmail.com>
2021-04-15 18:02:15 -0400

    added content to history.php

--- history.php
+++ history.php
@@ -6,28 +6,56 @@
     </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <?php include 'header.php'; ?>
     <div class="container">
-        <h1></h1>
-        <diva class="cards">
-            <div class="d-flex flex-row justify-content-center flex-wrap">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="title">
-                            <h5 class="card-title">Graphite</h5>
-                        </div>
-                        <p class="gen-text card-text">
-                            A graphite-moderated reactor is a nuclear reactor that uses carbon as a neutron moderator, which allows natural uranium to be used as nuclear fuel.
-                        </p>
-                        <div class="card-link">
-                            <a href="#" class="btn btn-dark">Learn More</a>
-                        </div>
-                    </div>
-                </div>
+        <div class="row modCover">
+            <div class="coverText">
+                <h1 class="text-light ">HISTORY</h1>
+                <p class="text-white-50 ">After World War II, the U.S. military sought other uses for nuclear reactor technology, there were political reasons to pursue civilian use of atomic energy </p>
             </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="gen-text">
+        <p>Chicago Pile-1 (CP-1) was the world's first artificial nuclear reactor. On 2 December 1942, the first human-made self-sustaining nuclear chain reaction was initiated in CP-1, during an experiment led by Enrico Fermi. The secret development of the reactor was the first major technical achievement for the Manhattan Project, the Allied effort to create atomic bombs during World War II.</p>
+        </div>
+        <div class="gen-text">
+        <p>In Chicago, Samuel K. Allison had found a suitable location 60 feet (18 m) long, 30 feet (9.1 m) wide and 26 feet (7.9 m) high, sunk slightly below ground level, in a space under the stands at Stagg Field originally built as a rackets court.[56][57] Stagg Field had been largely unused since the University of Chicago had given up playing American football in 1939,[47][58] but the rackets courts under West Stands were still used for playing squash and handball. Between 15 September and 15 November 1942, groups under Herbert Anderson and Walter Zinn constructed 16 experimental piles under the Stagg Field stands</p>
+        </div>
+        <div class="hist-img">
+            <img src="img/history_chicago_01.jpg" class="img-fluid">
+        </div>
+        <!-- second section -->
+        <div class="gen-text">
+        <p>On 25 June, the Army and the Office of Scientific Research and Development (OSRD) had selected a site in the Argonne Forest near Chicago for a plutonium pilot plant; this became known as "Site A". The risk of building an operational reactor running at criticality in a populated area was a significant issue, as there was a danger of a catastrophic nuclear meltdown blanketing one of the United States' major urban areas in radioactive fission products. But the physics of the system suggested that the pile could be safely shut down even in the event of a runaway reaction.</p>
+        </div>
+        <div class="gen-text">
+            <p>The reactors were used to undertake research related to weapons, such as investigations of the properties of tritium. Wartime experiments included measuring the neutron absorption cross-section of elements and compounds. Albert Wattenberg recalled that about 10 elements were studied each month, and 75 over the course of a year. Today the site of the old Stagg Field is occupied by the University's Regenstein Library, which was opened in 1970, and the Joe and Rika Mansueto Library, which was opened in 2011.[121] A Henry Moore sculpture, Nuclear Energy, stands in a small quadrangle just outside the Regenstein Library.[2] It was dedicated on 2 December 1967, to commemorate the 25th anniversary of CP-1 going critical.</p>
+        </div>
+        <div class="hist-img">
+            <img src="img/history_chicago_02.jpg" class="img-fluid">
+        </div>
+        <!-- third section -->
+        <div class="gen-text">
+            <p>"World's first nuclear power plant" is the claim made by signs at the site of the EBR-I, which is now a museum near Arco, Idaho. Originally called "Chicago Pile-4", it was carried out under the direction of Walter Zinn for Argonne National Laboratory.[15] This experimental LMFBR operated by the U.S. Atomic Energy Commission produced 0.8 kW in a test on 20 December 1951[16] and 100 kW (electrical) the following day,[17] having a design output of 200 kW (electrical).</p>
+        </div>
+        <div class="hist-img">
+            <img src="img/history_breeder_01.png" class="img-fluid">
+        </div>
+        <div class="gen-text">
+            <p>The design purpose of EBR-I was not to produce electricity but instead to validate nuclear physics theory that suggested that a breeder reactor should be possible. In 1953, experiments revealed the reactor was producing additional fuel during fission, thus confirming the hypothesis. On November 29, 1955, the reactor at EBR-I suffered a partial meltdown during a coolant flow test. The flow test was trying to determine the cause of unexpected reactor responses to changes in coolant flow. It was subsequently repaired for further experiments, which determined that thermal expansion of the fuel rods and the thick plates supporting the fuel rods was the cause of the unexpected reactor response</p>
+        </div>
+        <div class="hist-img">
+            <img src="img/history_breeder_02.jpg" class="img-fluid">
+        </div>
+        <div class="gen-text">
+            <p>Besides generating the world's first electricity from atomic energy, EBR-I was also the world's first breeder reactor and the first to use plutonium fuel to generate electricity (see also the Clementine nuclear reactor). EBR-1's initial purpose was to prove Enrico Fermi's fuel breeding principle, a principle that showed a nuclear reactor producing more fuel atoms than consumed. Along with generating electricity, EBR-1 would also prove this principle</p>
+        </div>
     </div>
     <?php include 'footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
